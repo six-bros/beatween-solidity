82cb476b9ee92aad72ec90cd9d4f71946cfad3f4
HJ <rlagudwns68@gmail.com>
2018-12-01 18:06:44 +0900

    edited

--- mixtape.sol
+++ mixtape.sol
@@ -3,26 +3,48 @@ pragma solidity >=0.4.25 <0.6.0;
 import '/ERC721/ERC721.sol'
 import './user.sol';
 
+pragma solidity >=0.4.25 <0.6.0;
+
+// import './user.sol';
+
 contract Mixtape {
-	address producer;
-	address rapper;
+    address payable producer;
+    address payable rapper;
 
-	uint clap;
-	uint playCnt;
+    uint public clapNum;
+    uint public downloadNum;
+    uint public totalBalance;
+    
+    mapping(address => bool) public downloaded;
+    mapping(address => uint) public clapped;
+    
+    constructor (address payable _producer, address payable _rapper) public {
+        producer = _producer;
+        rapper = _rapper;
+        clapNum = 0;
+        downloadNum = 0;
+    }
 
-	constructor (address _producer, address _rapper) public {
+    // modifier onlyRapper() {
+    //     require(address(msg.sender).isRapper[msg.sender]);
+    //     _;
+    // }
 
-	}
+    // function download() public {
+    //     require(downloaded[msg.sender] == false);
+    //     downloadNum++;
+    //     downloaded[msg.sender] = true;
+    // }
 
-	function download() public {
+    function giveClap() public {
+        require(clapped[msg.sender] < 50);
+        clapped[msg.sender]++;
+        clapNum++;
+    }
 
-	}
-
-	function giveClap() public {
-
-	}
-
-	function tip() public payable {
-
-	}
+    function tip() public payable {
+        totalBalance += msg.value;
+        address(producer).transfer(msg.value/2);
+        address(rapper).transfer(msg.value/2);
+    }
 }
