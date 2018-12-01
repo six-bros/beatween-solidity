f03056ffaa5b8506a39cace428640de11485334c
HJ <rlagudwns68@gmail.com>
2018-12-01 19:12:03 +0900

    edited

--- mixtape.sol
+++ mixtape.sol
@@ -3,19 +3,12 @@ pragma solidity >=0.4.25 <0.6.0;
 import './openZeppelin/ERC721/ERC721.sol';
 import './user.sol';
 
-pragma solidity >=0.4.25 <0.6.0;
-
-// import './user.sol';
-
 contract Mixtape {
     address payable producer;
     address payable rapper;
 
-    uint public clapNum;
-    uint public downloadNum;
     uint public totalBalance;
     
-    mapping(address => bool) public downloaded;
     mapping(address => uint) public clapped;
     
     constructor (address payable _producer, address payable _rapper) public {
@@ -25,17 +18,6 @@ contract Mixtape {
         downloadNum = 0;
     }
 
-    // modifier onlyRapper() {
-    //     require(address(msg.sender).isRapper[msg.sender]);
-    //     _;
-    // }
-
-    // function download() public {
-    //     require(downloaded[msg.sender] == false);
-    //     downloadNum++;
-    //     downloaded[msg.sender] = true;
-    // }
-
     function giveClap() public {
         require(clapped[msg.sender] < 50);
         clapped[msg.sender]++;
@@ -43,6 +25,7 @@ contract Mixtape {
     }
 
     function tip() public payable {
+		require(msg.value != 0);
         totalBalance += msg.value;
         address(producer).transfer(msg.value/2);
         address(rapper).transfer(msg.value/2);
