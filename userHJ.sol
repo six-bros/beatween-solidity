7b49ba541a474f964ea7c9d99107fad314c42390
HJ <rlagudwns68@gmail.com>
2018-12-01 23:26:43 +0900

    distribute

--- userHJ.sol
+++ userHJ.sol
@@ -9,6 +9,7 @@ contract Mixtape {
     uint public totalPlay;
 
     mapping(address => uint) numOfClap;
+    mapping(address => uint) multiSign;
     
     constructor(address payable _producer) public{
         producer = _producer;
@@ -17,8 +18,9 @@ contract Mixtape {
         totalPlay = 0;
     }
     
-    function registerRap(address payable _rapper) public {
+    function registerRap(address payable _rapper, uint uid) public {
         rapper.push(_rapper);
+        multiSign[_rapper] = uint256(keccak256(abi.encodePacked(_rapper, producer, uid)));
     }
     
     function saveTip(uint tip) public {
@@ -39,16 +41,28 @@ contract Mixtape {
     function play() public {
         totalPlay++;
     }
+    
+    function distribute(address payable _producer, address payable _rapper, uint uid) public {
+        require( uint256(keccak256(abi.encodePacked(_rapper, producer, uid))) == multiSign[_rapper]);
+        uint money = address(this).balance;
+        address(_producer).transfer(money/2);
+        address(_rapper).transfer(money/2);
+    }
+    
+    
+    
 }
 
 contract User {
 
     address public user;
     address[] public mixtapes;
+    uint uid;
     
 
-    constructor() public {
+    constructor(uint _uid) public {
         user = msg.sender;
+        uid = _uid;
     }
 
     function registerBeat() public {
@@ -58,7 +72,7 @@ contract User {
     }
 
     function registerRap(address mixtape) public {
-        mixtape.call(abi.encodeWithSignature("registerRap(address payable", msg.sender));
+        mixtape.call(abi.encodeWithSignature("registerRap(address payable, uint256)", msg.sender, uid));
     }
 
     function donation(address mixtape, uint rapperIndex) public payable{
@@ -73,5 +87,9 @@ contract User {
     
     function play(address mixtape) public {
         mixtape.call(abi.encodeWithSignature("play()"));
+    }
+    
+    function distribute(address mixtape, address _producer, address _rapper, uint _uid) public {
+        mixtape.call(abi.encodeWithSignature("distribute(address payable, address payable, uint256)", _producer, _rapper, _uid));
     }
 }
